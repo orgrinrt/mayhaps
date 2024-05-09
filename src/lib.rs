1bd9191671f431b619cb49ec45f600bab5e088a3
orgrinrt <ort@hiisi.digital>
2024-05-09 10:35:38 +0300

    refactor namings to make it more unlikely to pollute user namespace in case of ::* imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
-pub use paste::paste as maybe_paste;
+pub use paste::paste as __mayhaps_paste;
 
 #[macro_export]
 macro_rules! maybe {
     ($name:ident from ($opt_var:expr) exists $body:block) => {
-        $crate::maybe_paste! {
+        $crate::__mayhaps_paste! {
             let [< maybe_ $name >] = $opt_var;
             if let Some($name) = [< maybe_ $name >] $body
         }
     };
     ($name:ident from ($opt_var:expr) exists { $($body:tt)* } else $else_body:block) => {
-        $crate::maybe_paste! {
+        $crate::__mayhaps_paste! {
             let [< maybe_ $name >] = $opt_var;
             if let Some($name) = [< maybe_ $name >] $body $else_body
         }
